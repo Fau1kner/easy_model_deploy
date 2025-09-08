dafab0e1528d1c6f8f37c621ff1f6d3e70668f0f
Faulkner <folkner@ya.com>
2025-09-09 02:51:10 +0300

    init and train a model

---
+++ app_api.py
@@ -0,0 +1,58 @@
+'''
+Проверка работы API (/health)
+curl -X GET http://127.0.0.1:5000/health
+curl -X GET http://127.0.0.1:5000/stats
+curl -X POST http://127.0.0.1:5000/predict_model -H "Content-Type: application/json" -d "{\"Pclass\": 3, \"Age\": 22.0, \"Fare\": 7.2500}"
+'''
+
+from fastapi import FastAPI, Request, HTTPException
+import pickle
+import pandas as pd
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Загрузка модели из файла pickle
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Счетчик запросов
+request_count = 0
+
+# Модель для валидации входных данных
+class PredictionInput(BaseModel):
+    Pclass: int
+    Age: float
+    Fare: float
+
+@app.get("/stats")
+def stats():
+    return {"request_count": request_count}
+
+@app.get("/health")
+def health():
+    return {"status": "OK"}
+
+@app.post("/predict_model")
+def predict_model(input_data: PredictionInput):
+    global request_count
+    request_count += 1
+
+    # Создание DataFrame из данных
+    new_data = pd.DataFrame({
+        'Pclass': [input_data.Pclass],
+        'Age': [input_data.Age],
+        'Fare': [input_data.Fare]
+    })
+
+    # Предсказание
+    predictions = model.predict(new_data)
+
+    # Преобразование результата в человеко-читаемый формат
+    result = "Survived" if predictions[0] == 1 else "Not Survived"
+
+    return {"prediction": result}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=5000)
